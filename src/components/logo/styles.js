5f0ca4c54b3ba3d931247b7ac970ddf8ae213745
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2022-04-24 23:40:42 +0530

    first commit

---
+++ src/components/logo/styles.js
@@ -0,0 +1,13 @@
+import { styled } from "@mui/system";
+
+export const LogoContent = styled("div")({
+  minWidth: "fitContent",
+  alignItem: "ceneter",
+  display: "flex",
+  alignItems: "center",
+});
+
+export const LogoImage = styled("img")({
+  width: "1.5rem",
+  marginRight: "0.5rem",
+});
