ef9564af4ec3bbd760da9ee0fdc1f6fb7f662b57
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2022-04-25 17:34:10 +0530

    first commit

---
+++ src/components/addRemoveItemButton/styles.js
@@ -0,0 +1,25 @@
+import styled from "@mui/system/styled";
+
+export const AddRemoveButton = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  width: "100%",
+  justifyContent: "center",
+  borderRadius: "100px",
+  padding: "0.3rem",
+});
+
+export const QuantityPositive = styled("div")({
+  display: "flex",
+  width: "100%",
+  alignItems: "center",
+  justifyContent: "space-between",
+  marginRight: "10px",
+});
+
+export const QuantityNegative = styled("div")({
+  display: "flex",
+  width: "100%",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
